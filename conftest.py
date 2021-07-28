c5ba9be651a7431d8d7da71eccb0f03f79ad536f
Huỳnh Quang Thái <thai.huynh@pap-tech.com>
2021-07-28 15:29:22 +0700

    update code

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ test_path = os.path.dirname(__file__)
 test_report_path = test_path + '/reports'
 sys.path.append(test_path)
 sys.path.append(test_report_path)
-chrome_path = test_path + '/chromedriver'
+chrome_path = '/usr/lib/chromium-browser/chromedriver'
 
 @pytest.fixture(autouse=True)
 def chrome_driver():
@@ -20,7 +20,7 @@ def chrome_driver():
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--window-size=1920,1080")
     global driver
-    driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_path)
+    driver = webdriver.Chrome(executable_path=chrome_path,options=chrome_options)
     driver.implicitly_wait(10)
     driver.maximize_window()
     yield driver
